87a441c5933eba501cb3ef596ebc1939c6c551c3
Tolu <emoruwatoluwanimi@gmail.com>
2024-08-31 00:03:31 -0400

    Editing load speeds on the Homepage

--- cssa-website-react/src/pages/Home/Home.tsx
+++ cssa-website-react/src/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState , Suspense , useEffect } from 'react';
 import HomeLayer1 from './HomeLayer1';
-import HomeLayer2 from './HomeLayer2';
-import HomeLayer3 from './HomeLayer3';
-import HomeLayer4 from './HomeLayer4';
+// import HomeLayer2 from './HomeLayer2';
+// import HomeLayer3 from './HomeLayer3';
+// import HomeLayer4 from './HomeLayer4';
+const HomeLayer2 = React.lazy(() => import('./HomeLayer2'));
+const HomeLayer3 = React.lazy(() => import('./HomeLayer3'));
+const HomeLayer4 = React.lazy(() => import('./HomeLayer4'));
 
 export function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -15,13 +18,16 @@ export function getWindowDimensions() {
 const Home: React.FC = () => {
 
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
 
   return (
         <div className='home-layers'>
           <HomeLayer1 windowDimensions={windowDimensions}/>
-          <HomeLayer2 windowDimensions={windowDimensions}/>
-          <HomeLayer3 windowDimensions={windowDimensions}/>
-          <HomeLayer4 />
+          <Suspense fallback={<div>Loading...</div>}>
+            <HomeLayer2 windowDimensions={windowDimensions} />
+            <HomeLayer3 windowDimensions={windowDimensions} />
+            <HomeLayer4 />
+          </Suspense>
         </div>
   );
 };
